a6682f826914b053900e52062ff51df71cddcf13
jcarvajalh <caamiloc@outlook.com>
2022-05-21 15:54:50 -0500

    1 file modified

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,22 @@ const PORT = 4000;
 app.use(cors());
 app.use(bodyParser.json);
 
+//db concector
+const mongoose = require('mongoose');
+
+//express router
+const rtmRoutes = express.Router();
+
+//create db conecction:
+const conecction_string = '';
+mongoose.connect(conecction_string, { userNewUrlParser: true });
+const conecction = mongoose.connection;
+
+//2. rutas para la peticiones del cliente
+rtmRoutes.route('/CrearReq').post(function (req, res) {});
+rtmRoutes.route('/CrearDes').post(function (req, res) {});
+rtmRoutes.route('/AsignarReq').post(function (req, res) {});
+
 //server is listening on port 4000
 app.listen(PORT, function () {
   console.log('server está corriendo en puerto ' + PORT);
